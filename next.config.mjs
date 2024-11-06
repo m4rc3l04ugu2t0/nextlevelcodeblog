f36b8b4e26ad9dbd687f7cbf2e0f7c5c768033f7
Marcelo <nextlevlcode@gmail.com>
2024-11-06 11:24:18 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,17 +9,25 @@ const nextConfig = {
     ]
   },
   async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://nextlevelcodeblog.netlify.app" }, // especifique o domínio exato
-                    { key: "Access-Control-Allow-Methods", value: "GET, DELETE, PATCH, POST, PUT, OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-      ]
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://backend-newsletter.onrender.com", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 }
 
