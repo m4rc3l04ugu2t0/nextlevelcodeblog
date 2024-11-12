72bee9303a9fe9a5a9a919e32761e97990f1fc04
Marcelo <nextlevlcode@gmail.com>
2024-11-12 09:10:34 -0300

    style

--- src/app/posts/ArchLinuxInstallGuide/Header.tsx
+++ src/app/posts/ArchLinuxInstallGuide/Header.tsx
@@ -3,33 +3,23 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-      <header className="bg-gray-800 p-4">
-        <nav className="container mx-auto flex justify-between items-center">
+      <header className="bg-[#0f0f0f] p-4 shadow-lg border-b-2 border-gray-800">
           <Div className="responsive-reading text-white font-bold">
             Next Level Code
           </Div>
-          <ul className="flex space-x-4">
-            <li>
+          <nav className="container mx-auto flex justify-between items-center">
               <Link href="/" className="text-gray-300 hover:text-white">
                 Home
               </Link>
-            </li>
-            <li>
               <Link href="#footer" className="text-gray-300 hover:text-white">
                 Sobre
               </Link>
-            </li>
-            <li>
               <Link href="#" className="text-gray-300 hover:text-white">
                 Assinar
               </Link>
-            </li>
-            <li>
               <Link href="#footer" className="text-gray-300 hover:text-white">
                 Contato
               </Link>
-            </li>
-          </ul>
         </nav>
       </header>
   )
